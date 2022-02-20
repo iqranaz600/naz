f97e5d20b7f60d3780b3bbf6904c77b80f5f57f0
iqranaz600 <98934255+iqranaz600@users.noreply.github.com>
2022-02-20 01:46:23 -0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+void main(){
+print("hello");
+print("hello word");
+print("environmental\nvariable app is downloaded");
+print("life is very\t beautiful");
+// print("i live in hyderabad");
+String a=("object oriented program 4458");
+print(a);
+var b=("my country name is pakistan");
+print(b);
+int c=678;
+print(c);
+double a1=789.90;
+print(a1);
+num b1=789.9078;
+print(b1);
+bool c1=true;
+print(c1);
+bool a2=false;
+print(a2.runtimeType);
+List education=["primary",10,12,"degree"];
+print(education[0]);
+print(education[1]);
+print(education[2]);
+print(education[3]);
+var d="minal";
+var age=12;
+print("username:${d}\t\nage:${age}");
+int d1=8;
+int d2=6;
+int sum=d1+d2;
+print("sum of d1 and d2 is:${sum}");
+int e=7;
+int f=10;
+int sub=e-f;
+print("sub of ${e} and ${f} is:${sub}");
+int e1=6;
+int f1=8;
+int multi=e1*f1;
+print("multiplication of ${e1} and ${f1} is:${multi}");
+int g=6;
+int h=4;
+double div=g/h;
+print("divide of ${g} and ${h} is:${div}");
+int g1=5;
+int h1=2;
+int a4=g1%h1;
+print("reminder of ${g1} and ${h1} is:${a4}");
+var m=6;
+print(++m);
+int n=7;
+print(n++);
+print(n);
+int o=5;
+print(--o);
+var p=4;
+print(p--);
+print(p);
+
+
+
+
+
+
+
+
+
+
+}
